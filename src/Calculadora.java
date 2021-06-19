de2f9948cf353ac8101f397d121fcf862e05e637
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-19 12:13:05 -0300

    calculadora com funcoes

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,62 +2,59 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+
 		Scanner leitor = new Scanner(System.in);
 		double valor1 = 0, valor2=0;
-		double soma, subtracao, divisao, multiplicacao;
 		int opcao = 0;
 		
-		while (opcao!=6) {
+		while (opcao!=6) 
+		{
 			System.out.println("PROGRAMA CALCULADORA");
-			System.out.println("Escolha sua op��o!");
+			System.out.println("Escolha sua opção!\n");
 			System.out.println("1 - Digitar valores");
 			System.out.println("2 - Realizar soma");
-			System.out.println("3 - Realizar subtra��o");
-			System.out.println("4 - Realizar divis�o");
-			System.out.println("5 - Realizar multiplica��o");
+			System.out.println("3 - Realizar subtração");
+			System.out.println("4 - Realizar divisão");
+			System.out.println("5 - Realizar multiplicação");
 			System.out.println("6 - Sair");
 			opcao = leitor.nextInt();
 			
-			switch(opcao) {
-			case 1:
-				System.out.println("\n\nDigite o 1� valor:");
-				valor1 = leitor.nextDouble();
-				System.out.println("\n\nDigite o 2� valor:");
-				valor2 = leitor.nextDouble();
-				
-				System.out.println("Os valores " + valor1 + " e " + valor2 + " foram armazenados\n\n");
-				
+			switch(opcao) 
+			{
+				case 1:
+					System.out.println("\n\nDigite o 1 valor:");
+					valor1 = leitor.nextDouble();
+
+					System.out.println("\n\nDigite o 2 valor:");
+					valor2 = leitor.nextDouble();
+					
+					System.out.println("Os valores " + valor1 + " e " + valor2 + " foram armazenados\n\n");
 				break;
-			case 2:
-				System.out.println("\n\nRealizando a soma entre " + valor1 + " e " + valor2);
-				soma = valor1 + valor2;
-				System.out.println("O resultado foi " + soma + "!\n\n");
-				
+
+				case 2:
+					Funcoes.soma(valor1, valor2);
 				break;
-			case 3:
-				System.out.println("\n\nRealizando a subtra��o entre " + valor1 + " e " + valor2);
-				subtracao = valor1 - valor2;
-				System.out.println("O resultado foi " + subtracao + "!\n\n");
+
+				case 3:
+					Funcoes.subtracao(valor1, valor2);
 				break;
-			case 4:
-				System.out.println("\n\nRealizando a divis�o entre " + valor1 + " e " + valor2);
-				divisao = valor1 / valor2;
-				System.out.println("O resultado foi " + divisao + "!\n\n");
+
+				case 4:
+					Funcoes.divisao(valor1, valor2);
 				break;
-			case 5:
-				System.out.println("\n\nRealizando a multiplica��o entre " + valor1 + " e " + valor2);
-				multiplicacao = valor1 * valor2;
-				System.out.println("O resultado foi " + multiplicacao + "!\n\n");
+
+				case 5:
+					Funcoes.multiplicacao(valor1, valor2);
 				break;
-			case 6:
-				System.out.println("Saindo do sistema");
+
+				case 6:
+					System.out.println("Saindo do sistema");
 				break;
 			}
-			
 		}
-		leitor.close();
-		
-	}
 
+		leitor.close();
+	}
 }
--- src/Funcoes.java
+++ src/Funcoes.java
@@ -9,4 +9,62 @@ public class Funcoes
     {
         System.out.println("Bom dia " + nome);
     }
+
+    public static void exibirResultado(double resultado, String operacao)
+    {
+        System.out.println("O resultado da " + operacao + " foi " + resultado + "!\n\n");
+    }
+
+    public static void soma(double valor1, double valor2)
+    {
+        double resultado;
+
+        resultado = valor1 + valor2;
+
+        System.out.println("\n\nRealizando a soma entre " + valor1 + " e " + valor2);
+
+        exibirResultado(resultado, "SOMA");
+    }
+
+    public static void subtracao(double valor1, double valor2)
+    {
+        double resultado;
+
+        resultado = valor1 - valor2;
+
+        System.out.println("\n\nRealizando a substração entre " + valor1 + " e " + valor2);
+
+        exibirResultado(resultado, "SUBTRAÇÃO");
+    }
+
+    public static void divisao(double valor1, double valor2)
+    {
+        double resultado;
+
+        if (valor2 != 0)
+        {
+
+        resultado = valor1 / valor2;
+
+        System.out.println("\n\nRealizando a divisão entre " + valor1 + " e " + valor2);
+
+        exibirResultado(resultado, "DIVISÃO");
+        }
+        else
+        {
+            System.out.println("Não é possível dividir por ZERO.");
+        }
+    }
+
+    public static void multiplicacao(double valor1, double valor2)
+    {
+        double resultado;
+
+        resultado = valor1 * valor2;
+
+        System.out.println("\n\nRealizando a multiplicação entre " + valor1 + " e " + valor2);
+
+        exibirResultado(resultado, "MULTIPLICAÇÃO");
+    }
+
 }
